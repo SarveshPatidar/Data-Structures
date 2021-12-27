8a4891d20bafb6518a58e5333377fbd63e125266
sarvesh <sarvesh@sarveshs-mbp.lan>
2021-12-26 19:30:14 -0500

    HashSet Data structure implementation.

---
+++ Data-structures/src/com/datastructure/design/MyHashSet.java
@@ -0,0 +1,73 @@
+package com.datastructure.design;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class MyHashSet {
+	
+	private static final int  ARRAY_SIZE = 769;
+
+	// this list will store the hash key for the child list.
+	private List<List<Integer>> parentList;
+
+	// this constructor will initialize our parentList.
+	public MyHashSet() {
+		parentList = new ArrayList<>(ARRAY_SIZE);
+		for (int i = 0; i < ARRAY_SIZE; i++) {
+			parentList.add(null);
+		}
+	}
+
+	
+	// this method will add new element to our hash-set.
+	public void add(int key) {
+		int index = key % ARRAY_SIZE;
+		List<Integer> childlist = parentList.get(key);
+		if (childlist == null) {
+			List<Integer> list = new LinkedList<>();
+			list.add(key);
+			parentList.set(index, list);
+		} else {
+			if (!childlist.contains(key)) {
+				childlist.add(key);
+			}
+
+		}
+	}
+
+	// this method will remove element for the hash-set.
+	public void remove(int key) {
+		int index = key % ARRAY_SIZE;
+		List<Integer> childList = parentList.get(index);
+
+		if (childList != null) {
+			childList.remove(Integer.valueOf(key));
+		}
+	}
+	
+	// this method will check if a element exists in our hash-set or not.
+	public boolean contains(int key) {
+		int index = key % ARRAY_SIZE;
+		List<Integer> childList = parentList.get(index);
+		return childList!=null && childList.contains(key);
+	}
+
+	// driver method to test methods of MyHashSet class.
+	public static void main(String[] args) {
+		
+		MyHashSet myHashSet = new MyHashSet();
+		myHashSet.add(1);      // set = [1]
+		myHashSet.add(2);      // set = [1, 2]
+		myHashSet.contains(1); // return True
+		myHashSet.contains(3); // return False, (not found)
+		myHashSet.add(2);      // set = [1, 2]
+		myHashSet.contains(2); // return True
+		myHashSet.remove(2);   // set = [1]
+		myHashSet.contains(2); // return False, (already removed)
+
+	}
+
+
+}
